a71d42bcb170438b66a6ca4aec09f0504a8144f9
Berzok <34751752+Berzok@users.noreply.github.com>
2018-04-12 12:16:30 +0200

    Add files via upload

---
+++ Python_Test/Three_Data.py
@@ -0,0 +1,63 @@
+#coding: utf-8
+import numpy as np
+import random
+
+# sigmoid function
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+
+def aleaInt():
+    return random.randint(0, 3)
+
+def proba2answer(trainedData):
+    trainedData = np.around(trainedData, 0)
+    return nonlin(trainedData)
+    
+    
+# input dataset
+X = np.array([  [aleaInt(), aleaInt(), aleaInt()],
+                [aleaInt(), aleaInt(), aleaInt()],
+                [aleaInt(), aleaInt(), aleaInt()],
+                [aleaInt(), aleaInt(), aleaInt()] ])
+
+print "Input matrix: "
+print X
+
+# output dataset            
+y = np.array([[aleaInt(),aleaInt(),aleaInt(),aleaInt()]]).T
+
+print "Output matrix: "
+print y, "\n"
+
+# seed random numbers to make calculation
+# deterministic (just a good practice)
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1)) - 1
+
+for iter in xrange(10000):
+
+    # forward propagation
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+
+    # how much did we miss?
+    l1_error = y - l1
+    
+
+    # multiply how much we missed by the 
+    # slope of the sigmoid at the values in l1
+    l1_delta = l1_error * nonlin(l1,True)
+
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta)
+
+print "Output After Training:"
+print l1
+print "R�sulats obtenus:"
+print np.dot(l0,syn0)
+print "R�ponse fournie: "
+print proba2answer(l1)
